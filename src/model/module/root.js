1d41c22a07fc2f721224c5398fa25bf62f29f0c6
sansan <tracy541@126.com>
2019-04-20 10:46:16 +0800

    feat: main layout

--- src/model/module/root.js
+++ src/model/module/root.js
@@ -5,8 +5,8 @@
 export default {
   namespace: 'root',
   state: {
-    antdLang: 'zh_CN', // antd的组件国际化
-    localLang: 'zh', // 本地站点的国际化
+    antdLang: 'zh_CN', // antd的组件国际化，zh_CN中文，en_GB英文
+    localLang: 'zh', // 本地站点的国际化， zh-中文，en-英文
     theme: 'default', // 主题设置： default默认，后续再更新
   },
   subscriptions: {
